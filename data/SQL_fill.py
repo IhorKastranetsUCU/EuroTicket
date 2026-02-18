d4496b1194529cbdc10afcfcbbef36de7b78ac64
IhorKastranetsUCU <kastranets.pn@ucu.edu.ua>
2026-02-18 23:20:18 +0200

    Some more data for test

--- data/SQL_fill.py
+++ data/SQL_fill.py
@@ -40,7 +40,6 @@ class Trip(Base):
 
     train = relationship("Train", back_populates="trips")
     stops = relationship("RouteStop", back_populates="trip", order_by="RouteStop.stop_order")
-    exceptions = relationship("CalendarException", back_populates="trip")
 
 
 class RouteStop(Base):
@@ -48,7 +47,7 @@ class RouteStop(Base):
 
     id = Column(Integer, primary_key=True)
     trip_id = Column(Integer, ForeignKey('trips.id'), index=True)
-    station_id = Column(Integer, ForeignKey('stations.id'))
+    station_id = Column(Integer, ForeignKey('stations.id'), index=True)
 
     arrival_time = Column(Time)
     departure_time = Column(Time)
@@ -57,15 +56,6 @@ class RouteStop(Base):
     trip = relationship("Trip", back_populates="stops")
     station = relationship("Station", back_populates="stops")
 
-
-class CalendarException(Base):
-    __tablename__ = 'calendar_exceptions'
-
-    id = Column(Integer, primary_key=True)
-    trip_id = Column(Integer, ForeignKey('trips.id'))
-    date = Column(Date, nullable=False)
-    is_running = Column(Boolean, nullable=False)
-    trip = relationship("Trip", back_populates="exceptions")
 
 class Graph(Base):
     __tablename__ = "graph"
@@ -101,10 +91,113 @@ class StationFill(DBFill):
 
         self.commit()
 
+from datetime import datetime
+
+def parse_time(time_str):
+    if not time_str:
+        return None
+    return datetime.strptime(time_str, "%H:%M").time()
+
+
+class TrainFill(DBFill):
+    def fill_from_json(self, filepath):
+        with open(filepath, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        for number, train_info in data.items():
+            train = Train(
+                number=number,
+                name=train_info.get("name"),
+                has_wifi=train_info.get("has_wifi", False),
+                has_air_con=train_info.get("has_AC", False),
+                has_restaurant=train_info.get("has_restaurant", False),
+                has_bycycle_holder=train_info.get("has_bicycle", False),
+                is_accessible=train_info.get("accessible", False),
+            )
+            self.session.add(train)
+
+        self.commit()
+
+
+class TripFill(DBFill):
+    def fill_from_json(self, filepath):
+        with open(filepath, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        for number, train_info in data.items():
+            train = self.session.query(Train).filter_by(number=number).first()
+            if not train:
+                continue
+
+            trip = Trip(
+                train_id=train.id,
+                days_mask=train_info.get("day_mask", 127),
+            )
+            self.session.add(trip)
+
+        self.commit()
+
+
+class RouteStopFill(DBFill):
+    def fill_from_json(self, filepath):
+        with open(filepath, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        for number, train_info in data.items():
+            train = self.session.query(Train).filter_by(number=number).first()
+            if not train:
+                continue
+
+            trip = self.session.query(Trip).filter_by(train_id=train.id).first()
+            if not trip:
+                continue
+
+            stations_data = train_info.get("stations", [])
+            total = len(stations_data)
+
+            for station_info in stations_data:
+                order = station_info.get("orderNumber")
+                station_name = station_info.get("stationName")
+
+                station = self.session.query(Station).filter_by(name=station_name).first()
+                if not station:
+                    continue
+
+                # Перша зупинка має лише departure, остання лише arrival
+                arrival = None
+                departure = None
+
+                if order == 1:
+                    departure = parse_time(station_info.get("departureTime"))
+                elif order == total:
+                    arrival = parse_time(station_info.get("arrivalTime"))
+                else:
+                    arrival = parse_time(station_info.get("arrivalTime"))
+                    departure = parse_time(station_info.get("departureTime"))
+
+                route_stop = RouteStop(
+                    trip_id=trip.id,
+                    station_id=station.id,
+                    arrival_time=arrival,
+                    departure_time=departure,
+                    stop_order=order,
+                )
+                self.session.add(route_stop)
+
+        self.commit()
+
+
+
+
 if __name__ == "__main__":
-    engine = create_engine("sqlite:///EuroTicket.db")
+    engine = create_engine("sqlite:///EuroTicket1.db")
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
+
     StationFill(session).fill_from_json("railway_stations.json")
+    TrainFill(session).fill_from_json("structure.json")
+    TripFill(session).fill_from_json("structure.json")
+    RouteStopFill(session).fill_from_json("structure.json")
+
     session.close()
